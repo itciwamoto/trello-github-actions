6e6d4c91468c0e3fb43f27258fe3f8c3e4f029d6
itc-iwamoto <t_iwamoto@itccl.jp>
2022-06-20 09:50:25 +0900

    no message

--- index.js
+++ index.js
@@ -5,55 +5,34 @@ try {
   const apiKey = process.env.TRELLO_API_KEY;
   const apiToken = process.env.TRELLO_API_TOKEN;
   const boardId = process.env.TRELLO_TEST_BOARD_ID;
+  const labelName = process.env.TRELLO_LABEL_NAME;
   const refContext = process.env.GITHUB_REF_CONTEXT;
-  const action = 'add_labels_when_push';
 
-  console.log(apiKey);
-  console.log(apiToken);
-  console.log(boardId);
-  console.log(refContext);
-  console.log(action);
+  addLabelsAngular(apiKey, apiToken, boardId, refContext, labelName);
 
-  switch (action) {
-    case 'add_labels_when_push':
-      addLabelsAngular(apiKey, apiToken, boardId, refContext);
-      break;
-
-  }
 } catch (error) {
   core.setFailed(error.message);
 }
 
-function addLabelsAngular(apiKey, apiToken, boardId, refContext) {
-  console.log(refContext);
+function addLabelsAngular(apiKey, apiToken, boardId, refContext, labelName) {
   const departureListId = process.env.TRELLO_TEST_LIST1_ID;
-  console.log(departureListId);
   const ynumber = refContext.match(/y\d{4}/) != null ?  refContext.match(/y\d{4}/)[0] : '10000';
-  console.log(ynumber);
   const snumber = refContext.match(/s\d{4}/) != null ?  refContext.match(/s\d{4}/)[0] : '10000';
 
-  console.log(snumber);
   const trelloLabelIds = [];
   getLabelsOfBoard(apiKey, apiToken, boardId).then(function(response) {
     const trelloLabels = response;
       trelloLabels.forEach(function(trelloLabel) {
-  console.log(trelloLabel.name);
-        if (trelloLabel.name == 'Angular') {
+        if (trelloLabel.name == labelName) {
           trelloLabelIds.push(trelloLabel.id);
         }
       });
 
-  console.log(trelloLabelIds);
       getCardsOfList(apiKey, apiToken, departureListId).then(function(cards) {
         let cardId;
         cards.forEach(function(card) {
-    console.log(card.name);
-    console.log(card.name.match(/y\d{4}/));
-    console.log(card.name.match(/s\d{4}/));
           const card_ynumber = card.name.match(/y\d{4}/) != null ? card.name.match(/y\d{4}/)[0] : '20000';
-    console.log(card_ynumber);
           const card_snumber = card.name.match(/s\d{4}/) != null ? card.name.match(/s\d{4}/)[0] : '20000';
-    console.log(card_snumber);
           if (card_ynumber == ynumber || card_snumber == snumber) {
             cardId = card.id;
             return true;
@@ -62,7 +41,6 @@ function addLabelsAngular(apiKey, apiToken, boardId, refContext) {
         const cardParams = {
           labelIds: trelloLabelIds.join()
         }
-    console.log(cardParams);
         if (cardId) {
           addLabelsCard(apiKey, apiToken, cardId, cardParams).then(function(response) {
           });
@@ -93,7 +71,6 @@ function addLabelsCard(apiKey, apiToken, cardId, params) {
 }
 
 function getLabelsOfBoard(apiKey, apiToken, boardId) {
-  console.log(apiKey,apiToken,boardId);
   return new Promise(function(resolve, reject) {
     request(`https://api.trello.com/1/boards/${boardId}/labels?key=${apiKey}&token=${apiToken}`)
       .then(function(body) {
