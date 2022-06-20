b0f3ef43bbf4ae3433e08ce2c2d978cee56da48c
itc-iwamoto <t_iwamoto@itccl.jp>
2022-06-20 11:23:37 +0900

    no message

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ function addLabelsAngular(apiKey, apiToken, boardId, refContext, labelName) {
   const ynumber = refContext.match(/y\d{4}/) != null ?  refContext.match(/y\d{4}/)[0] : '10000';
   const snumber = refContext.match(/s\d{4}/) != null ?  refContext.match(/s\d{4}/)[0] : '10000';
 
-  const trelloLabelIds = [];
+  const tempTrelloLabelIds = [];
   getLabelsOfBoard(apiKey, apiToken, boardId).then(function(response) {
     const trelloLabels = response;
       trelloLabels.forEach(function(trelloLabel) {
         if (trelloLabel.name == labelName) {
-          trelloLabelIds.push(trelloLabel.id);
+          tempTrelloLabelIds.push(trelloLabel.id);
         }
       });
 
@@ -36,12 +36,18 @@ function addLabelsAngular(apiKey, apiToken, boardId, refContext, labelName) {
           if (card_ynumber == ynumber || card_snumber == snumber) {
             console.log(card);
             cardId = card.id;
+            card.idLabels.forEach(function (cardLabel) {
+              tempTrelloLabelIds.push(cardLabel);
+             });
             return true;
           }
         });
+
+        const trelloLabelIds = Array.from(new Set(tempTrelloLabelIds));
         const cardParams = {
           labelIds: trelloLabelIds.join()
         }
+        console.log(cardParams);
         if (cardId) {
           addLabelsCard(apiKey, apiToken, cardId, cardParams).then(function(response) {
           });
